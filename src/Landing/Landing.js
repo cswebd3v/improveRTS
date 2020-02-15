e152bf733b9375cb749eb2b7f1d33b58c91779f7
unknown <cswebd3v@gmail.com>
2020-02-14 20:37:58 -0600

    styles

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -12,7 +12,10 @@ class Landing extends React.Component {
                     <Highlight p="If you are a strategy gamer, you know RTS games can be extremely complex. improveRTS focuses specifically on videos that can be studied and analyzed in order to learn and improve. Currently, two RTS games are supported: StarCraft II and WarCraft III, with many more to come in the future." headline="Knowledge is Power. Power is Everything." />
                     <Highlight headline="CHOOSE YOUR GAME" p="Select your favorite game to enter the video log and see results catered to you. Right now, the supported games are Starcraft II and Warcraft III, with more to come in the near future." />
                     <Highlight headline="CHOOSE YOUR MATCHUP" p="Having trouble with a specific faction matchup? Tired of losing to Zerg? Choose the matchup that you want to study, and get to work." />
-                    <a href="/"><Highlight headline="Get Started NOW" /></a>
+                    <div className="highlight">
+                        <a href="/"><button>GET STARTED NOW</button></a>                     
+                    
+                    </div>
                 </div>
             </div>
         )
